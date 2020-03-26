35cfb1d9d807dd87a63c7bcfc3f6a9c8e3b95fea
Martyna Plomecka <martyna.plomecka@uzh.ch>
2020-03-26 15:18:01 +0100

    multivariate model

---
+++ Useful/multivariate_model.R
@@ -0,0 +1,32 @@
+library(brms)
+library(ggplot2)
+library(tidyverse)
+
+#Load the data:
+tbl<- read.table("/Users/mplome/dev/STAGE2/Data/full_data_agg.csv",
+                      header = TRUE, sep=",")
+head(tbl)
+tbl$id <- factor(tbl$sbj_id)
+tbl$typ <- factor(tbl$typ)
+tbl$age <- factor(tbl$age)
+tbl$test_num <- factor(tbl$test_num)
+
+final_with_er = tbl %>% 
+  group_by(blok) %>% 
+  mutate(error_rate =  error/(error+correct))
+view(final_with_er)
+
+final_with_er = write.csv(final_with_er,"/Users/mplome/dev/STAGE2/Data/full_data_agg_with_er.csv" )
+final_with_er<- read.table("/Users/mplome/dev/STAGE2/Data/full_data_agg_with_er.csv",
+                      header = TRUE, sep=",")
+
+all <- brm(mvbind(error_rate, rt, gain, peak_velocity) ~1+ age + typ + (1|sbj_id),
+           data = final_with_er)
+
+#glmerror <- brm(correct | trials(correct + error) ~ 1 + typ + (1|sbj_id),
+                # data = tbl, family = binomial(link='probit'))
+
+brms::marginal_effects(all)
+plot(all)
+summary(all)
+brms::pp_check(all)
