c66f48f598556bbeb02a777b46c291c1b950c64f
Martyna Plomecka <martyna.plomecka@uzh.ch>
2020-03-28 12:50:47 +0100

    merge age, updated

--- Preparation_code/merge_age.R
+++ Preparation_code/merge_age.R
@@ -1,15 +1,8 @@
-#for later
-# define cut offs
-#subjects_100_800 = subjects[(subjects$sacc_time>100 )& (subjects$sacc_time<800) , ]
-#subjects_100_800
+#28.03.20 - everything works here
 
-
-#20.03 - wsszytsko dziala, wiek sie merguje odpowiednip
 library(readxl)
 
-
-#table <- read.table("/Users/mplome/data/full_data_for_2_stage.csv", header = TRUE, sep=",")
- table <- read.table("/Users/mplome/dev/STAGE2/Data/raw.csv", header = TRUE, sep=",")
+table <- read.table("/Users/mplome/dev/STAGE2/Data/raw.csv", header = TRUE, sep=",")
 ids <- read_excel("/Users/mplome/dev/STAGE2/Data/TRT_old.xlsx")
 
 table$age = NaN
