ba30238f2cf5c252266b216d24a807de57fd4286
Martyna Plomecka <martyna.plomecka@uzh.ch>
2020-03-31 13:01:08 +0200

    ICC for ER

--- reciprobit+icc/ICC for error rates.R
+++ reciprobit+icc/ICC for error rates.R
@@ -1,39 +1,77 @@
-#tuttaj irr tylko dla error rate
-
+#IRR for error rate 
 library(dplyr)
 library(irr)
 #load data
-najlepszaagg = read.csv("/Users/mplome/dev/STAGE2/full_data_agg.csv")
+najlepszaagg = read.csv("/Users/mplome/dev/STAGE2/Data/full_data_agg.csv")
 head(najlepszaagg)
 oldagg = najlepszaagg[najlepszaagg$age==1,]
-youngagg= najlepszaagg[najlepszaagg$age==0,]
+yngagg= najlepszaagg[najlepszaagg$age==0,]
 
-head(youngagg)
-head(oldagg)
-######################preparation error rate#icc 1 z wikipedii
-eprotestoldagg = oldagg[oldagg$typ=="p" & oldagg$test_num==1, c("sbj_id", "error")]   
-eproretestoldagg = oldagg[oldagg$typ=="p" &oldagg$test_num==2, c("sbj_id", "error")] 
-eprotrtoldagg <- merge(eprotestoldagg,  eproretestoldagg, by = c("sbj_id"))
+#old
+protestoldagg = oldagg[oldagg$typ=="p" & oldagg$test_num==1, ]
+antitestoldagg = oldagg[oldagg$typ=="a" & oldagg$test_num==1, ]
+proretestoldagg = oldagg[oldagg$typ=="p" & oldagg$test_num==2, ]
+antiretestoldagg = oldagg[oldagg$typ=="a" & oldagg$test_num==2, ]
+
+#yng
+protestyngagg = yngagg[yngagg$typ=="p" & yngagg$test_num==1, ]
+antitestyngagg = yngagg[yngagg$typ=="a" & yngagg$test_num==1, ]
+proretestyngagg = yngagg[yngagg$typ=="p" & yngagg$test_num==2, ]
+antiretestyngagg = yngagg[yngagg$typ=="a" & yngagg$test_num==2, ]
+
+####################################
+############old#####################
+####################################
+#test
+mprotestoldagg = protestoldagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+mantitestoldagg = antitestoldagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+#retest
+mproretestoldagg = proretestoldagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+mantiretestoldagg = antiretestoldagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+####################################
+############yng#####################
+####################################
+#test
+mprotestyngagg = protestyngagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+mantitestyngagg = antitestyngagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+#retest
+mproretestyngagg = proretestyngagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+mantiretestyngagg = antiretestyngagg %>%
+  group_by(sbj_id)%>%
+  summarise(sum_error = sum(error))
+
+######################preparation error rate#icc 1 from wiki
+eprotrtoldagg <- merge(mprotestoldagg,  mproretestoldagg, by = c("sbj_id"))
 eiccprotrtoldagg = icc(type = "agreement", ratings = (eprotrtoldagg[, c(2,3)]))
 eiccprotrtoldagg
 
-
-eantitestoldagg= oldagg[oldagg$typ=="a" &oldagg$test_num==1, c("sbj_id", "error") ]   
-eantiretestoldagg= oldagg[oldagg$typ=="a" &oldagg$test_num==2, c("sbj_id", "error")] 
-eantitrtoldagg <- merge(eantitestoldagg,  eantiretestoldagg, by = c("sbj_id"))
+eantitrtoldagg <- merge(mantitestoldagg,  mantiretestoldagg, by = c("sbj_id"))
 eiccantitrtoldagg = icc(type = "agreement", ratings = (eantitrtoldagg[, c(2,3)]))
 eiccantitrtoldagg
 
-eprotestyoungagg= youngagg[youngagg$typ=="p" &youngagg$test_num==1, c("sbj_id", "error") ]   
-eproretestyoungagg= youngagg[youngagg$typ=="p" &youngagg$test_num==2, c("sbj_id", "error")] 
-eprotrtyoungagg <- merge(eprotestyoungagg,  eproretestyoungagg, by = c("sbj_id"))
+eprotrtyoungagg <- merge(mprotestyoungagg,  mproretestyoungagg, by = c("sbj_id"))
 eiccprotrtyoungagg = icc(type = "agreement", ratings = (eprotrtyoungagg[, c(2,3)]))
 eiccprotrtyoungagg
 
-eantitestyoungagg= youngagg[youngagg$typ=="a" &youngagg$test_num==1, c("sbj_id", "error") ]   
-eantiretestyoungagg= youngagg[youngagg$typ=="a" & youngagg$test_num==2, c("sbj_id", "error")] 
-eantitrtyoungagg <- merge(eantitestyoungagg, eantiretestyoungagg, by = c("sbj_id"))
+eantitrtyoungagg <- merge(mantitestyoungagg, mantiretestyoungagg, by = c("sbj_id"))
 eiccantitrtyoungagg = icc(type = "agreement", ratings = (eantitrtyoungagg[, c(2,3)]))
-
-
 eiccantitrtyoungagg
