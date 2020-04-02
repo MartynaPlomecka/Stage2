841c04b6c01f9da6a8699118713adced9fc35c01
Martyna Plomecka <martyna.plomecka@uzh.ch>
2020-04-02 22:37:42 +0200

    multiv_model

--- Stats/multivariate_model.R
+++ Stats/multivariate_model.R
@@ -1,3 +1,5 @@
+
+
 library(brms)
 library(ggplot2)
 library(tidyverse)
@@ -21,22 +23,27 @@ View(final_with_er)
 
 #final_with_er = write.csv(final_with_er,"/Users/mplome/dev/STAGE2/Data/full_data_agg_with_er.csv" )
 
+
+prior<-c(set_prior("cauchy(0, 10)",class = "b", coef = ""),set_prior("cauchy(0, 10)", class = "Intercept", coef = ""))
+
 all <- brm(mvbind(error_rate, rt, gain, peak_velocity) ~1+ age + typ + (1|sbj_id),
           prior = prior, data = final_with_er)
 
 
 summary(all, priors = TRUE, prob = 0.987) #after correcting for multiple comparisons with the nyholt approach, for details see:Stats ->Nyholt
 
-prior<-c(set_prior("cauchy(0, 10)",class = "b", coef = ""),set_prior("cauchy(0, 10)", class = "Intercept", coef = ""))
 
 
 
+brms::conditional_effects(all)
 
 brms::marginal_effects(all)
 plot(all)
 summary(all)
-#brms::pp_check(all)
-
+brms::pp_check(all, resp = "rt")
+brms::pp_check(all, resp = "errorrate")
+brms::pp_check(all, resp = "peakvelocity")
+brms::pp_check(all, resp = "gain")
 
 
 
